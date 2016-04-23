29f1b5ef7fa25ab96f6f3c75e71768fe0a6911c9
Manju <bvmanjunathrao@gmail.com>
2016-04-23 15:28:48 -0400

    Initial support for Lint

--- jakefile.js
+++ jakefile.js
@@ -1,12 +1,14 @@
-task("default",[],function(){
-  console.log("default");
+task("default",["lint"]);
+desc("Lint everthing");
+task("lint",[],function(){
+  var lint = require("./build/lint/lint_runner.js");
+  lint.validateFile("jakefile.js", {}, {});
 });
-
-desc("Example!");
-task("example",["dependency"],function(){
-  console.log("example task.");
-});
-
-task("dependency",function(){
-  console.log("dependency task")
-});
+// desc("Example!");
+// task("example",["dependency"],function(){
+//   console.log("example task.");
+// });
+//
+// task("dependency",function(){
+//   console.log("dependency task")
+// });
