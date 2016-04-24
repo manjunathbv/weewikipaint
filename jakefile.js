eb5341e2cadad7fbbd4f0d2cbedb3725a098d213
Manju <bvmanjunathrao@gmail.com>
2016-04-23 21:54:19 -0400

    Configured jake to run lint on all js files

--- jakefile.js
+++ jakefile.js
@@ -1,14 +1,28 @@
+/*global desc, task, jake, fail, complete */
+
+"use strict";
+
 task("default",["lint"]);
+
 desc("Lint everthing");
 task("lint",[],function(){
   var lint = require("./build/lint/lint_runner.js");
-  lint.validateFile("jakefile.js", {}, {});
+
+  var files = new jake.FileList();
+  files.include("**/*.js");
+  files.exclude("node_modules");
+  files.exclude("build");
+
+  var options = {
+    node: true
+  };
+
+  var globals ={
+    describe: false,
+    it: false,
+    beforeEach: false,
+    afterEach: false
+  };
+
+  lint.validateFileList(files.toArray(), options, globals);
 });
-// desc("Example!");
-// task("example",["dependency"],function(){
-//   console.log("example task.");
-// });
-//
-// task("dependency",function(){
-//   console.log("dependency task")
-// });
